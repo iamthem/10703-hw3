bafa1239d75576bc3909ae4313cb5b9b4ec6e2f5
iamthem <lslopez@andrew.cmu.edu>
2021-11-04 09:08:32 -0400

    Initial commit Q3

---
+++ p3-templates/playground.py
@@ -0,0 +1,18 @@
+# %%
+from collections import OrderedDict 
+import gym
+from gym import spaces
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import GCBC 
+from importlib import reload
+reload(GCBC)
+
+# %%
+# build env
+l, T = 5, 30
+env = GCBC.FourRooms(l, T)
+### Visualize the map
+env.render_map()
+
